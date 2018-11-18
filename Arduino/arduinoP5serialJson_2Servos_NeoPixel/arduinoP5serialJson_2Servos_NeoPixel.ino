737441b52bed59cc2a3b7aae7b83ff21eb7b0c14
April De Zen <aprildezen@Aprils-MacBook-3.local>
2018-11-18 15:46:28 -0500

    arduino code

---
+++ Arduino/arduinoP5serialJson_2Servos_NeoPixel/arduinoP5serialJson_2Servos_NeoPixel.ino
@@ -0,0 +1,46 @@
+/*
+ * Creation & Computation - Digital Futures, OCAD University
+ * Kate Hartman / Nick Puckett
+ * 
+ * Send 2 values to P5.js
+ * values are formatted as JSON objects
+ * This example reads 2 values from p5 and outputs to led brightness
+ * 
+ * 
+ */
+#include <ArduinoJson.h>
+#include <Servo.h>
+//*****BE SURE TO INSTALL VERSION 5.13.3 OF THE LIBRARY IT INSTALL V6BETA BY DEFAULT 
+
+Servo myServo1; 
+Servo myServo2; 
+
+int SERVO1 = 11;
+int SERVO2 = 12;
+int LEDpin1 = 10;
+
+int p5Input1;                                   //these variables hold the input values
+int p5Input2;
+
+
+void setup() 
+{
+  Serial.begin(9600);                                     //turn on the serial port
+  pinMode(LEDpin1,OUTPUT);                       //  
+myServo1.attach(SERVO1);
+myServo2.attach(SERVO2);
+}
+
+void loop() 
+{
+  DynamicJsonBuffer messageBuffer(200);                   //create the Buffer for the JSON object        
+  JsonObject& p5Read = messageBuffer.parse(Serial);      //create a JsonObject variable and attach it to incoming Serial messages     
+ 
+  p5Input1 = p5Read["somethingsomething"];
+  p5Input2 = p5Read["led2"];
+
+  analogWrite(LEDpin1,p5Input1);
+  mySerco1.write(p5Input1);
+  mySerco1.write(p5Input2);
+
+}
